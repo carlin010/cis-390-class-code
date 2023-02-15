1f470b746b263ec658dc31c8fdf2325e2f80dd37
Nate Carlin <carlin010@gannon.edu>
2023-02-15 17:58:07 -0500

    add thread2, sem, mutex

--- thread1.c
+++ thread1.c
@@ -1,29 +1,48 @@
+// Run with cc -D_REENTRANT thread1.c -lpthread
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+// new thead library
 #include <pthread.h>
 
-void *thread_function();
-int count = 0;
+// prototype of function called by thead
+void *thread_function(void *arg);
+
+// globally defined variable
+char message[] = "Hello World";
 
 int main()
 {
-    pthread_t a_thread, b_thread;
-    pthread_create(&a_thread, NULL, thread_function, NULL);
-    pthread_create(&a_thread, NULL, thread_function, NULL);
-
-    pthread_join(a_thread, NULL);
-    pthread_join(b_thread, NULL);
-    printf("%d\n",count);
-    return 0;
+    int res;
+    pthread_t a_thread;
+    void *thread_result;
+    // start running the thread
+    res = pthread_create(&a_thread, NULL, thread_function, (void *)message);
+    if (res != 0)
+    {
+        perror("Thread creation failed");
+        exit(EXIT_FAILURE);
+    }
+    printf("Waiting for thread to finish...\n");
+    // wait for thread to finish execution (similar to wait)
+    res = pthread_join(a_thread, &thread_result);
+    if (res != 0)
+    {
+        perror("Thread join failed");
+        exit(EXIT_FAILURE);
+    }
+    printf("Thread joined, it returned %s\n", (char *)thread_result);
+    printf("Message is now %s\n", message);
+    exit(EXIT_SUCCESS);
 }
 
-void *thread_function()
+void *thread_function(void *arg)
 {
-    for (int i = 0; i < 1000000; i++) {
-        // lock
-        count++;
-        // unlock 
-    }
+    printf("thread_function is running. Argument was %s\n", (char *)arg);
+    sleep(3);
+    // write new source "bye" to destination mem
+    strcpy(message, "Bye!");
+    pthread_exit("Thank you for the CPU time");
 }
